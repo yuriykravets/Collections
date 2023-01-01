02dbfba4eed7222074320e05c95d13363ccaf55d
Yuriy Kravets <97830497+yuriykravets@users.noreply.github.com>
2023-01-02 01:19:54 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
+    println("mShoppingList original: $mShoppingList")
+    val extraShopping = listOf("Cookies", "Sugar", "Eggs")
+    mShoppingList.addAll(extraShopping)
+    println("mShoppingList items added: $mShoppingList")
+    if (mShoppingList.contains("Tea")) {
+        mShoppingList[mShoppingList.indexOf("Tea")] = "Coffee"
+    }
+    mShoppingList.sort()
+    println("mShoppingList sorted: $mShoppingList")
+    mShoppingList.reverse()
+    println("mShoppingList reversed: $mShoppingList")
+
+    val mShoppingSet = mShoppingList.toMutableSet()
+    println("mShoppingSet: $mShoppingSet")
+    val moreShopping = setOf("Chives", "Spinach", "Milk")
+    mShoppingSet.addAll(moreShopping)
+    println("mShoppingSet items added: $mShoppingSet")
+    mShoppingList = mShoppingSet.toMutableList()
+    println("mShoppingList new version: $mShoppingList")
+}
